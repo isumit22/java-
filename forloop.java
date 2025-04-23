07edf1fdfd8d476368d66edf99d7b59786005145
isumit22 <139735352+isumit22@users.noreply.github.com>
2025-04-23 12:21:20 +0530

    for

--- forloop.java
+++ forloop.java
@@ -1,4 +1,8 @@
 import java.util.*;
 public class forloop {
-    
+    public static void main(String args[]) {
+        for (int j=0; j<=20; j++) {
+            System.out.println( j );
+    }
+}
 }
