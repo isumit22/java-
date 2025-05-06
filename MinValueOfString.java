bad52b9105cfd52983e487ea532b7cab351f86a2
isumit22 <139735352+isumit22@users.noreply.github.com>
2025-05-06 12:41:11 +0530

    dsa1

---
+++ MinValueOfString.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class MinValueOfString {
+    public static int minValue(String str, int k) {
+        int[] freq = new int[26];
+        for (char ch : str.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int f : freq) {
+            if (f > 0) {
+                maxHeap.add(f);
+            }
+        }
+
+        while (k > 0 && !maxHeap.isEmpty()) {
+            int top = maxHeap.poll();
+            top--;
+            k--;
+            if (top > 0) {
+                maxHeap.add(top);
+            }
+        }
+
+        int result = 0;
+        while (!maxHeap.isEmpty()) {
+            int val = maxHeap.poll();
+            result += val * val;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minValue("abeee", 1));            
+        System.out.println(minValue("aabccbcbcabee", 3));   
+    }
+}
